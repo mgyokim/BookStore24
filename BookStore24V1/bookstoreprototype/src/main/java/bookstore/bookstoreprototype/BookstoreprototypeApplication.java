627a0aca8040500c655d6041cc0e22abb6611ddc
mgkim-developer <mgkim.developer@gmail.com>
2023-04-29 16:21:27 +0900

    2023.04.29_(v1 프로젝트 환경 설정 완료)

---
+++ BookStore24V1/bookstoreprototype/src/main/java/bookstore/bookstoreprototype/BookstoreprototypeApplication.java
@@ -0,0 +1,13 @@
+package bookstore.bookstoreprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreprototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreprototypeApplication.class, args);
+	}
+
+}
