6036ddb330427dc393492a01773cc89353912aab
mgkim-developer <mgkim.developer@gmail.com>
2023-05-09 20:41:44 +0900

    2023.05.09_(v1 주문 검색 기능 개발)

---
+++ BookStore24V1/bookstoreprototype/src/main/java/bookstore/bookstoreprototype/repository/OrderSearch.java
@@ -0,0 +1,13 @@
+package bookstore.bookstoreprototype.repository;
+
+import bookstore.bookstoreprototype.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+
+    private String memberName;  // 회원 이름
+    private OrderStatus orderStatus;  // 주문 상태[ORDER, CANCEL]
+}
