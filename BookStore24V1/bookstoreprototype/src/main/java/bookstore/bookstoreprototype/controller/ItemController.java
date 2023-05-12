c9ab60551e00e429d7bb4022f813e51e6ae1d02b
mgkim-developer <mgkim.developer@gmail.com>
2023-05-12 16:25:09 +0900

    2023.05.12_(v1 updateItem 파라미터 수정)

--- BookStore24V1/bookstoreprototype/src/main/java/bookstore/bookstoreprototype/controller/ItemController.java
+++ BookStore24V1/bookstoreprototype/src/main/java/bookstore/bookstoreprototype/controller/ItemController.java
@@ -75,7 +75,7 @@ public class ItemController {
     }
 
     @PostMapping("items/{itemId}/edit")
-    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+    public String updateItem(@ModelAttribute("form") BookForm form) {
 
         Book book = new Book();
         book.setId(form.getId());
