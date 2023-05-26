a87bebd8130795820a072066cf04aed137d5f9bf
mgkim-developer <mgkim.developer@gmail.com>
2023-05-26 16:55:46 +0900

    2023.05.26_(v1_2 간단한 주문 조회 V3:엔티티를 DTO로 변환 - 페치 조인 최적화)

--- BookStore24V1_2/bookstoreprototype/src/main/java/bookstore/bookstoreprototype/api/OrderSimpleApiController.java
+++ BookStore24V1_2/bookstoreprototype/src/main/java/bookstore/bookstoreprototype/api/OrderSimpleApiController.java
@@ -58,6 +58,20 @@ public class OrderSimpleApiController {
         return result;
     }
 
+    /**
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * - fetch join 으로 쿼리 1번 호출
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
     @Data
     private class SimpleOrderDto {
         private Long orderId;
