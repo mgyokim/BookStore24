d54ba408e3c6bd91cb28bfec90ba1638c6f49937
mgkim-developer <mgkim.developer@gmail.com>
2023-06-22 20:45:41 +0900

    BookStore24V2 프로젝트 생성

---
+++ BookStore24V2/v2/src/main/java/bookstore24/v2/V2Application.java
@@ -0,0 +1,13 @@
+package bookstore24.v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V2Application.class, args);
+	}
+
+}
---
+++ BookStore24V2/v2/src/test/java/bookstore24/v2/V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package bookstore24.v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
