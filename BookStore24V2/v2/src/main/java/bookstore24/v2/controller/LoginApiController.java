791989f2a68004921dca1f2adbc90bb2d1cf3af2
mgkim-developer <mgkim.developer@gmail.com>
2023-07-14 18:13:26 +0900

    [Feat] : 네이버 인가 코드 받기 완료
    네이버 인가 코드 받기 기능 구현함.

--- BookStore24V2/v2/src/main/java/bookstore24/v2/controller/LoginApiController.java
+++ BookStore24V2/v2/src/main/java/bookstore24/v2/controller/LoginApiController.java
@@ -1,7 +1,9 @@
 package bookstore24.v2.controller;
 
 import bookstore24.v2.config.oauth.logic.KakaoLogic;
+import bookstore24.v2.config.oauth.logic.NaverLogic;
 import bookstore24.v2.config.oauth.token.KakaoOauthToken;
+import bookstore24.v2.config.oauth.token.NaverOauthToken;
 import bookstore24.v2.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginApiController {
 
     private final KakaoLogic kakaoLogic;
+    private final NaverLogic naverLogic;
 
     @GetMapping("/auth/kakao/callback")
     public @ResponseBody
@@ -29,6 +32,23 @@ public class LoginApiController {
 
         // 회원의 LoginId 반환
         return member.getLoginId();
+    }
+
+    @GetMapping("auth/naver/callback")
+    public @ResponseBody
+    String naverLogin(String code) {
+
+        // 발급받은 인가 코드로 토큰 요청
+
+        // 발급받은 액세스토큰으로 프로필 정보 요청
+
+        // 해당 회원의 회원가입 여부 체크후 비회원만 회원가입 처리
+
+        // 해당 회원 로그인 처리
+
+        // 회원의 LoginId 반환
+
+        return "네이버 인증 완료 : 코드값 : " + code;
     }
 }
 
