176b33e1871f6ecc6e9ba1c6e83cd2bad7408973
mgkim-developer <mgkim.developer@gmail.com>
2023-08-03 16:10:08 +0900

    [Rename] : saveNicknameAndResidence 메서드를 이용할 때 사용되는 DTO 의 네이밍을 메서드명이랑 통일되도록 수정함
    saveNicknameAndResidence 메서드를 이용할 때 사용되는 DTO 의 네이밍을 메서드명이랑 통일되도록 수정했음.

--- BookStore24V2/v2/src/main/java/bookstore24/v2/member/dto/SaveNicknameAndResidenceRequestDto.java
+++ BookStore24V2/v2/src/main/java/bookstore24/v2/member/dto/SaveNicknameAndResidenceRequestDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import javax.validation.constraints.NotNull;
 
 @Data
-public class NicknameResidenceSaveRequestDto {
+public class SaveNicknameAndResidenceRequestDto {
 
     @NotNull
     private String nickname;
--- BookStore24V2/v2/src/main/java/bookstore24/v2/member/dto/SaveNicknameAndResidenceResponseDto.java
+++ BookStore24V2/v2/src/main/java/bookstore24/v2/member/dto/SaveNicknameAndResidenceResponseDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import javax.validation.constraints.NotNull;
 
 @Data
-public class NicknameResidenceSaveResponseDto {
+public class SaveNicknameAndResidenceResponseDto {
 
     @NotNull
     private String loginId;
